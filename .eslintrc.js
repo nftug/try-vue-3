a119f9350394dec61d408d9ad9a281e233447d69
nftug <whitepony1214@gmail.com>
2022-04-13 16:58:55 +0900

    ESLintを設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
   env: {
-    node: true,
+    browser: true,
     es2021: true,
   },
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['vue', '@typescript-eslint'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
   extends: [
-    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
